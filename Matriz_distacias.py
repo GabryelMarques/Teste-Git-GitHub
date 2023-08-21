11507b4d31c70c1ea9271de3c0579bcc26d151ca
Gabryel Marques <gabryelmarques17@gmail.com>
2023-08-21 00:33:10 -0300

    Nova função adicionar aresta

--- Matriz_distacias.py
+++ Matriz_distacias.py
@@ -4,4 +4,7 @@ class Grafo:
         self.vertices = vertices
         self.grafo = [[0]*self.vertices for i in range(self.vertices)]
 
-   
+    def adiciona_aresta(self,u,v,peso ):
+        self.grafo[u-1][v-1] = peso #trocar = por += ser for grafo mùltiplo
+        self.grafo[v-1][u-1] = peso 
+        
