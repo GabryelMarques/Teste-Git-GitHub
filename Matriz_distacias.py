1e35760ee73628f91a176893ed830fc3cee914b4
Gabryel Marques <gabryelmarques17@gmail.com>
2023-08-21 00:15:01 -0300

    Nova inicialização da da matriz do grafo

--- Matriz_distacias.py
+++ Matriz_distacias.py
@@ -2,7 +2,6 @@ class Grafo:
 
     def __init__(self, vertices):
         self.vertices = vertices
-        self.grafo = [4][4]
-        #self.grafo = [[0]*self.vertices for i in range(self.vertices)]
+        self.grafo = [[0]*self.vertices for i in range(self.vertices)]
 
    
